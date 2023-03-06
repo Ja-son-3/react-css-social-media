9ef77579463f2353ccbbf02d44dfabb32fdacb7b
Jason Huang <jason97521@gmail.com>
2023-03-06 07:35:08 -0500

    top bar and side bar done

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,25 @@
+import "./post.css"
+import {MoreVert} from '@mui/icons-material'
+
+export default function Post() {
+  return (
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+                    <span className="postUsername">Jason Huang</span>
+                    <span className="postDate">5 mins ago</span>
+                </div>
+                <div className="postTopRight">
+                    <MoreVert />
+                </div>
+            </div>
+            <div className="postCenter">
+                <span className="postText"></span>
+            </div>
+            <div className="postBottom"></div>
+        </div>
+    </div>
+  )
+}
